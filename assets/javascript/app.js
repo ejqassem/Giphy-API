38ffba4a281776555a6ef4850c81122ee5667487
Eyad Qassem <eyadqassem@Eyads-MacBook-Air.local>
2017-03-25 20:40:48 -0400

    added sessionStorage capability for new buttons. JS Logic is complete. Needs more styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,78 @@
+var topics = ["Psych", "Scrubs", "Suits", "White Collar", "Hawaii Five O", "Silicon Valley", "The Walking Dead", "Game of Thrones", "Criminal Minds", "Fresh Prince of Bel-air"];
+var gifClicked = false;
+var counter = 0;
+var gifCounter = 0;
+
+for (var i = 0; i < sessionStorage.length; i++) {
+  topics.push(sessionStorage.getItem("newButton" + i));
+}
+
+for (var i = 0; i < topics.length; i++) {
+
+  var newButton = $("<button>");
+  newButton.attr("class", "shows");
+  newButton.attr("data-show", topics[i]);
+  newButton.html(topics[i]);
+  $("#buttonsHere").append(newButton);
+
+}
+
+$("#formButton").on("click", function(event) {
+  event.preventDefault();
+  var submitInfo = $("#newButtonText").val().trim();
+  sessionStorage.setItem("newButton" + counter, submitInfo);
+  topics.push(submitInfo);
+  var newButton = $("<button>");
+  newButton.attr("class", "shows");
+  newButton.attr("data-show", submitInfo);
+  newButton.html(submitInfo);
+  $("#buttonsHere").append(newButton);
+  counter++;
+});
 
 $(document).on("click", ".shows", function(event) {
+
   event.preventDefault();
+  gifClicked = true;
+  if (gifClicked) {
+    $("#gifsHere").empty();
+  }
 
   var show = $(this).attr("data-show");
-  console.log($(this));
+  queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-          show + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-  $.ajax({url: queryURL, method:"GET"}).done(function(response) {
-
+  $.ajax({url: queryURL, method: "GET"}).done(function(response) {
     var results = response.data;
-          //cycles through the array of the objects returned from the ajax request
-        for (var i = 0; i < results.length; i++) {
-
-          var gifDiv = $("<div class='gifs'>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var personImage = $("<img>");
-
-          personImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-
-          gifDiv.prepend(personImage);
-
-          $("#gifsHere").prepend(gifDiv);
+    for (i = 0; i < results.length; i++) {
+      var newDiv = $("<div>");
+      newDiv.css("display", "inline-block");
+      var newImg = $("<img>");
+      var p = $("<p>");
+      newImg.attr("src", results[i].images.fixed_height_still.url);
+      newImg.attr("data-still", results[i].images.fixed_height_still.url);
+      newImg.attr("data-animate", results[i].images.fixed_height.url);
+      newImg.attr("data-state", "still");
+      newImg.addClass("gifs");
+      p.text("Rating: " + results[i].rating);
+      p.attr("class", "ratings");
+      p.css({"vertical-align": "middle", "text-align":"center"});
+      newDiv.append(p);
+      newDiv.append(newImg);
+      $("#gifsHere").append(newDiv);
     }
 
   });
 
 });
 
-$("#formButton").on("click", function(event) {
-  event.preventDefault();
-
-  var button = $("#newButtonText").val().trim();
-
-  var newButton = $("<button>");
-
-  newButton.addClass("shows");
-
-  newButton.attr("data-show", button);
-
-  newButton.html(button);
-
-  console.log(newButton);
-
-  $("#buttonsHere").prepend(newButton);
-
-  $("#newButtonText").val("");
-
-});
+$(document).on("click", ".gifs", function() {
+    console.log($(this).attr("data-state"));
+      var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
